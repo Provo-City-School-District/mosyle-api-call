90de85a38f77d95e445bfe2e78981a6f07192508
Espi <joshespi@gmail.com>
2023-05-25 10:29:15 -0600

    initial upload
    proof of concept while the new MDM is being evaluated.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# mosyle-api-call
-calls information from mosyle api.
+# Moysle API Call For the Vault
+
+pulls columns 'serial_number','date_checkin', 'installed_memory','cpu_model','osversion' from the listdevices endpoint.
+
+proof of concept while mosyle system being evaulated. 
+
+
+
+build container with ```docker build -t moysle_api .``` inside the directory
+
+run with ```docker run --rm --name moysle_api  moysle_api ```
+
+you'll need to provide a .env file with the following variables
+```
+MOSYLEU=
+MOSYLEP=
+MOSYLEAPI=
+```
---
+++ moysle_api.php
@@ -0,0 +1,38 @@
+<?php
+//credentails for the vault
+// $vUser = $_ENV['VUSER'];
+// $vPass = $_ENV['VPASS'];
+// $vLoc = $_ENV['VLOC'];
+// $vdata = $_ENV['VDATA'];
+
+//mosyle credentials
+$username = $_ENV['MOSYLEU'];
+$password = $_ENV['MOSYLEP'];
+
+
+
+$endpoint = '/listdevices';
+$parameters = array(
+    'accessToken' => $_ENV['MOSYLEAPI'],
+    'options' => array(
+      'os' => 'mac',
+      'specific_columns' => array('serial_number','date_checkin', 'installed_memory','cpu_model','osversion')
+    )
+  );
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://managerapi.mosyle.com/v2' . $endpoint); // start the curl
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return the transfer as a string
+curl_setopt($ch, CURLOPT_POST, true); // set as post request
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Basic '.base64_encode($username.':'.$password)));
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('content' => json_encode($parameters)));
+
+$output = curl_exec($ch); // $output contains the output string
+
+// close curl resource to free up system resources
+curl_close($ch);
+
+
+
+
+echo $output;
